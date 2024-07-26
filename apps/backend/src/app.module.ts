b5e7bfcbd13d331357358be64b75cc431d76ccdb
Marco Santarelle da Silva Jardim <944513@lojasrenner.com.br>
2024-07-26 16:02:24 -0300

    feat: add agendamento page using api

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
 import { ServicoModule } from './servico/servico.module';
 import { AgendamentoModule } from './agendamento/agendamento.module';
 
 @Module({
-  imports: [DbModule, ServicoModule, AgendamentoModule],
+  imports: [ConfigModule.forRoot(), DbModule, ServicoModule, AgendamentoModule],
   controllers: [],
   providers: [],
 })
---
+++ apps/frontend/src/components/agendamento/DataInput.tsx
@@ -0,0 +1,23 @@
+import DiaInput from "./DiaInput";
+import HorariosInput from "./HorariosInput";
+
+export interface DataInputProps {
+  data: Date;
+  quantidadeDeSlots: number;
+  dataMudou: (data: Date) => void;
+}
+
+export default function DataInput(props: DataInputProps) {
+  const { data, quantidadeDeSlots, dataMudou } = props;
+
+  return (
+    <div className="flex flex-col gap-10">
+      <DiaInput data={data} dataMudou={dataMudou} />
+      <HorariosInput
+        data={data}
+        qtdeHorarios={quantidadeDeSlots}
+        dataMudou={dataMudou}
+      />
+    </div>
+  );
+}
